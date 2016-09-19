d2af93ba72dabf7f5dba9f7be146376582cec595
Andrew Casner <a.casner@comcast.net>
2016-09-18 22:05:34 -0600

    Scaffold out header file with functions and structs for HashMap

--- HashMap.h
+++ HashMap.h
@@ -1,2 +1,24 @@
 // Andrew Casner KPCB Engineering Fellows Appliction Fall 2016
 
+#include <stdlib>
+#include <iostream>
+#include "HashMap.cc"
+
+// Class HashMap
+class HashMap {
+	public:
+		HashMap(int);
+		~HashMap();
+		bool set(std::string, int*);
+		int* get(std::string);
+		int* deleteNode(std::string);
+		float load();
+
+	private:
+		struct mapNode(){
+			std::string key;
+			int value;
+			int *head;
+			int *tail;
+		}
+}
