f8fc6db9dbbe024a9788bbecebd5b575d7133118
Andrew Casner <a.casner@comcast.net>
2016-09-18 22:19:01 -0600

    Created HashMap.cc and added functions in

---
+++ HashMap.cc
@@ -0,0 +1,25 @@
+// Andrew Casner HashMap.cc class file
+
+HashMap::HashMap(){
+
+}
+
+HashMap::~HashMap(){
+
+}
+
+bool HashMap::set(std::string keyIN, int * valueIN){
+
+}
+
+int* HashMap::get(std::string keyIN){
+
+}
+
+int* HashMap::deleteNode(std::string keyIN){
+
+}
+
+float HashMap::load(){
+
+}
